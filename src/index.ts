53b7caa97a4bd3191c5817a10c21ff59b8a725e7
Szymon Fryc <sz.fryc1@gmail.com>
2024-03-08 10:43:01 +0100

    Adding new functionality

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import { userRouter } from './api/user/user-router';
 import { clientRouter } from './api/client/client-router';
 import { trainerRouter } from './api/trainer/trainer-router';
 import { adminRouter } from './api/admin/admin-router';
-import { availabilityRouter } from './api/trainerAvailability/availability-router';
+import { availabilityRouter } from './api/availability/availability-router';
+import { bookingRouter } from './api/booking/booking-router';
+import { errorHandler } from './utils/global-error-handler';
 
 mongoose.connect('mongodb://127.0.0.1:27017/fitness-app', {})
   .then(() => {
@@ -21,10 +24,13 @@ app.get('/', (req, res) => {
   });
 
 app.use(express.json());
+app.use(userRouter);
 app.use(clientRouter);
 app.use(trainerRouter);
 app.use(adminRouter);
 app.use(availabilityRouter);
+app.use(bookingRouter);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is up on port ${ port }`);
